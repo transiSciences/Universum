78df7b381aec2c8a19b770180f0e61973eedbe5e
transiSciences <37506275+transiSciences@users.noreply.github.com>
2018-05-26 12:15:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const Client = new Discord.Client();
 const prefix = '=';
 const name = 'Universum';
-const version = 'Alpha 0.0.1;
+const version = 'Alpha 0.0.1';
 const lang = 'fr';
 
 Client.login(process.env.TOKEN)
